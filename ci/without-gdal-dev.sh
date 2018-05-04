a6ce1c73caffc8a4273ef633f6068ca5b1774fdd
Ari Jolma <ari.jolma@gmail.com>
2018-05-04 10:36:04 +0300

    sudo perl may have different @INC than perl

--- ci/without-gdal-dev.sh
+++ ci/without-gdal-dev.sh
@@ -5,6 +5,6 @@ sudo cpanm --installdeps --notest git://github.com/shawnlaffan/perl-alien-gdal
 sudo cpanm --notest Alien::Build::MM
 git clone --depth=50 --branch=master https://github.com/shawnlaffan/perl-alien-gdal.git 
 cd perl-alien-gdal
-perl Makefile.PL
-make | perl -ne 'BEGIN {$|=1; open our $log, ">", "build.log"}; print "\n" if 0 == ($. % 90); print "."; print {$log} $_;' || cat build.log
+sudo perl Makefile.PL
+sudo make | sudo perl -ne 'BEGIN {$|=1; open our $log, ">", "build.log"}; print "\n" if 0 == ($. % 90); print "."; print {$log} $_;' || cat build.log
 sudo make install
