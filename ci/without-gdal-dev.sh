0ebcfd92d5785786f5fbe44b4a860fd09c3fc1be
Ari Jolma <ari.jolma@gmail.com>
2018-05-04 11:24:02 +0300

    No sudo in perl related stuff.

--- ci/without-gdal-dev.sh
+++ ci/without-gdal-dev.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-sudo apt-get install cpanminus
-sudo cpanm --installdeps --notest git://github.com/shawnlaffan/perl-alien-gdal
-sudo cpanm --notest Alien::Build::MM
+cpanm --installdeps --notest git://github.com/shawnlaffan/perl-alien-gdal
+cpanm --notest Alien::Build::MM
 git clone --depth=50 --branch=master https://github.com/shawnlaffan/perl-alien-gdal.git 
 cd perl-alien-gdal
-sudo perl Makefile.PL
-sudo make | sudo perl -ne 'BEGIN {$|=1; open our $log, ">", "build.log"}; print "\n" if 0 == ($. % 90); print "."; print {$log} $_;' || cat build.log
-sudo make install
+perl Makefile.PL
+make | perl -ne 'BEGIN {$|=1; open our $log, ">", "build.log"}; print "\n" if 0 == ($. % 90); print "."; print {$log} $_;' || cat build.log
+make install
