834a8ac7933835b7991747f85abc78292128a78a
Shawn Laffan <shawnlaffan@gmail.com>
2019-04-16 09:52:43 +1000

    The Alien::gdal build handles long log lines under CI
    Remove double handling of line aggregation so travis
    sees progress and does not time out.

--- ci/without-gdal-dev.sh
+++ ci/without-gdal-dev.sh
@@ -8,7 +8,7 @@ cpanm --notest Alien::Build::MM
 git clone --depth=50 --branch=master https://github.com/shawnlaffan/perl-alien-gdal.git 
 cd perl-alien-gdal
 perl Makefile.PL
-make | perl -ne 'BEGIN {$|=1; open our $log, ">", "build.log"}; print "\n" if 0 == ($. % 90); print "."; print {$log} $_;' || cat build.log
+make
 make install
 cd ..
 rm -rf perl-alien-gdal
