fdb3c0c0dd4d3a10fff9cc6b5cf58b90e5a8e836
Shawn Laffan <shawnlaffan@gmail.com>
2019-04-16 11:22:40 +1000

    Let the cpanm call handle the prereqs when building gdal
    The more recent Alien::gdal releases on cpan
    should have the necessary metadata.

--- ci/without-gdal-dev.sh
+++ ci/without-gdal-dev.sh
@@ -6,12 +6,12 @@ cpanm --notest Sort::Versions
 #  SWL - this gets picked up later, but avoid throwing an error now
 cpanm --notest Alien::geos::af
 
-cpanm --installdeps --notest git://github.com/shawnlaffan/perl-alien-gdal
-cpanm --notest Alien::Build::MM
-git clone --depth=50 --branch=master https://github.com/shawnlaffan/perl-alien-gdal.git 
-cd perl-alien-gdal
-perl Makefile.PL
-make
-make install
-cd ..
-rm -rf perl-alien-gdal
+#cpanm --installdeps --notest git://github.com/shawnlaffan/perl-alien-gdal
+#cpanm --notest Alien::Build::MM
+#git clone --depth=50 --branch=master https://github.com/shawnlaffan/perl-alien-gdal.git 
+#cd perl-alien-gdal
+#perl Makefile.PL
+#make
+#make install
+#cd ..
+#rm -rf perl-alien-gdal
