b6fd1149f3a41aa9e1988375d7b940ead775bb26
Shawn Laffan <shawnlaffan@gmail.com>
2019-04-16 11:10:20 +1000

    Ensure Alien::geos::af is installed early

--- ci/without-gdal-dev.sh
+++ ci/without-gdal-dev.sh
@@ -3,6 +3,9 @@
 # bug in Alien::gdal Makefile.PL requires
 cpanm --notest Sort::Versions
 
+#  SWL - this gets picked up later, but avoid throwing an error now
+cpanm --notest Alien::geos::af
+
 cpanm --installdeps --notest git://github.com/shawnlaffan/perl-alien-gdal
 cpanm --notest Alien::Build::MM
 git clone --depth=50 --branch=master https://github.com/shawnlaffan/perl-alien-gdal.git 
