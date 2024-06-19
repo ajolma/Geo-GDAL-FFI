479f9ba654360ed14c9775c7a2fb5fa7f4f2462f
shawnlaffan <shawnlaffan@gmail.com>
2024-06-19 13:39:59 +1000

    post-release version bump

--- lib/Geo/GDAL/FFI.pm
+++ lib/Geo/GDAL/FFI.pm
@@ -26,7 +26,7 @@ use Geo::GDAL::FFI::GeomFieldDefn;
 use Geo::GDAL::FFI::Feature;
 use Geo::GDAL::FFI::Geometry;
 
-our $VERSION = 0.1200;
+our $VERSION = 0.1300;
 our $DEBUG = 0;
 
 our @ISA = qw(Exporter);
